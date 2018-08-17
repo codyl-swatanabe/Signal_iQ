1fb2fc60d6f47caae4395060680f035b63e0c5cf
Satoshi Watanabe <swatanabe@codyltech.com>
2018-08-17 23:29:04 +0900

    i

--- python/SignaliQ/client.py
+++ python/SignaliQ/client.py
@@ -56,7 +56,6 @@ class Client(object):
 
         :returns Bool: True if connection and channel is established successfully.
         """
-        __log__.info("############### Connection called.")
         params = self._build_connection_params()
 
         self._connection = amqp.Connection(**params)
